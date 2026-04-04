a59d661695c5d58d0b7639a3162fd14237c8d6eb
TRẦN NGUYỄN GIA BẢO <baot63610@gmail.com>
2026-04-04 20:35:06 +0700

    Add files via upload

---
+++ Roomanager/RoomateManager/Models/Member.cs
@@ -0,0 +1,16 @@
+﻿namespace RoommateManager.Models
+{
+    public enum MemberRole { Manager, Member }
+    public enum PaymentStatus { Paid, Unpaid, NotApplicable }
+
+    public class Member
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string AvatarInitial => Name?.Length > 0 ? Name[0].ToString().ToUpper() : "?";
+        public MemberRole Role { get; set; }
+        public PaymentStatus PaymentStatus { get; set; }
+        public decimal Amount { get; set; }
+        public bool IsCurrentUser { get; set; }
+    }
+}
